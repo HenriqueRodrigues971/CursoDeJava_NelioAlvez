63c93883b286bb7040ccbab8313fdc4d74eaec35
HenriqueRodrigues971 <kingofsneaker123@gmail.com>
2024-06-29 17:46:51 -0300

    up

--- src/Secao16_JogoDeXadres/boardgame/Tabuleiro.java
+++ src/Secao16_JogoDeXadres/boardgame/Tabuleiro.java
@@ -6,6 +6,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new BoardException("Erro na criação do tabuleiro: é necessario que haja uma linha e uma coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -15,27 +18,45 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExiste(linha, coluna)) {
+			throw new BoardException("Não tem posição no tabuleiro");
+		}
+
 		return pecas[linha][coluna];
 	}
+
 	public Peca peca(Position position) {
+		if (!posicaoExiste(position)) {
+			throw new BoardException("Não tem posição no tabuleiro");
+		}
 		return pecas[position.getLinha()][position.getColuna()];
 	}
-	
+
 	public void posicionarPeca(Peca peca, Position position) {
+		if (temUmaPeca(position)) {
+			throw new BoardException("Já existe uma peça na posição: " + position);
+		}
 		pecas[position.getLinha()][position.getColuna()] = peca;
 		peca.position = position;
+	}
+
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+
+	public boolean posicaoExiste(Position position) {
+		return posicaoExiste(position.getLinha(), position.getColuna());
+	}
+
+	public boolean temUmaPeca(Position position) {
+		if (!posicaoExiste(position)) {
+			throw new BoardException("Não tem posição no tabuleiro");
+		}
+		return peca(position) != null;
 	}
 }
