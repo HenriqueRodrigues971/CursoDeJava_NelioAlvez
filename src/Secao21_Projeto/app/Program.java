3bca432ffea785293016bf0d29e60800d271be60
HenriqueRodrigues971 <kingofsneaker123@gmail.com>
2024-07-27 20:07:56 -0300

    up jdbc course

--- src/Secao21_Projeto/app/Program.java
+++ src/Secao21_Projeto/app/Program.java
@@ -9,10 +9,11 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("==== Teste 1: Seller findById ===");
+
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
